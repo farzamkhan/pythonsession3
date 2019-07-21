338df7f66db5d1b19c051f9cbea3b7a4ccd18b27
farzamkhan <farzam@live.com>
2019-07-21 11:54:25 +0430

    change l by L

--- conditions.py
+++ conditions.py
@@ -1,2 +1,30 @@
-if 1:
-    print("hi")
+s = "lorem ipsum is simply dummy text of the printing and typesetting industry. \
+Lorem ipsum \nhas been the industry's standard dummy text ever since the 1500s.\
+when an unknown\nprinter took a gallery of type and scrambled it to make a \
+type specimen book. It has survived not only five centuries, but also \
+the leap into electronic typesetting,\nremaining essentially unchanged."
+
+if s.find("printing") != -1 :
+    new_s = s.replace("printing", "publishing")
+else:
+    new_s = s
+print(new_s)
+print("\n", "\n", "=============================")
+
+string_list  = s.split()
+new_string_list = []
+for i in range (0, len(string_list)):
+    word = string_list[i]
+    temp_list = []
+    
+    if word[0] == "l":
+        temp_list = list(word)
+        temp_list[0] = "L"
+        word = ''.join(temp_list)
+        new_string_list.append(word)
+    else:
+        new_string_list.append(word)
+
+print(' '.join(new_string_list))
+
+
